bb58f46a3013c498cb97825dcdea93250beff5e8
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2020-08-16 16:13:45 +0530

    Instead of using StdOut.printf we are using System.out.print

--- Charge.java
+++ Charge.java
@@ -5,7 +5,7 @@ public class Charge{
 
 	//Constructor
 	public Charge(double x0, double y0, double q0){
-		rx = xo;
+		rx = x0;
 		ry = y0;
 		q = q0;
 	}
@@ -29,6 +29,6 @@ public class Charge{
 		Charge c2 = new Charge(0.13, 0.94, 81.9);
 		double v1 = c1.potentialAt(x, y);
 		double v2 = c2.potentialAt(x ,y);
-		stdOut.printf("%.2e\n" (v1 + v2));
+		System.out.printf("%.2e\n", (v1 + v2));
 	}
 }
